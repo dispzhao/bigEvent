cb11391203922c76c76951be580c3ea299b2d9ba
lvshuai <www.2577677467@qq.com>
2022-07-31 12:22:02 +0800

    index分支开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,18 @@
 // 每次发起请求都会调用此函数，拿到ajax的配置对象，以此可以拼接url地址
 $.ajaxPrefilter(function (options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // 统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+        // 统一挂载complete回调函数
+        options.complete = function (res) {
+            // 不能没有登陆直接访问index页面
+            if (res.responseJSON.status === 1) {
+                localStorage.removeItem('token')
+                location.href = './login.html'
+            }
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+    getUserInfo();
+    // 获取用户信息函数
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) return layui.layer.msg('获取用户信息失败')
+                //渲染用户信息
+                readerAvatar(res.data);
+            }
+        })
+    }
+    // 渲染函数
+    function readerAvatar(user) {
+        // 1.获取用户昵称
+        let names = user.nickname || user.username
+        $('.welcome').html('欢迎&nbsp;&nbsp;' + names);
+        // 2.获取用户头像
+        if (user.user_pic == null) {
+            // 渲染文字头像
+            $('.layui-nav-img').hide()
+            let first = names[0].toUpperCase()
+            $('.text-avatar').html(first).show()
+        } else {
+            // 渲染图片头像
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+        }
+    }
+    // 为退出按钮绑定点击事件
+    $('#btn-loginout').on('click', function () {
+        //提示用户是否退出提示框
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //清空local中的token
+            localStorage.removeItem('token')
+            location.href = './login.html'
+            layer.close(index);
+        });
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
